ffc1a6e5388f837798c9d48da1777755cd012a3c
skydream <skydream2016@gmail.com>
2021-11-09 10:37:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function even_or_odd(number) {
+//     if (number % 2 == 0) {
+//       return 'Even';
+//     } else {
+//       return 'Odd';
+//     }
+//   }
+//   console.log(even_or_odd(176));
+
+// arr = [0, 1, -1, 3, 5, 15, 47, -37, -23];
+// arr_1 = [0, 1, -1, 3, 6];
+
+// function positiveSum(arr) {
+//   let sum = 0;
+//   for (let i =  0; i < arr.length; i++)
+//     if (arr[i] >= 0) sum += arr[i];
+//   return sum;
+// }
+// console.log(positiveSum(arr_1));
