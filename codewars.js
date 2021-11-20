c79c61df4a3901917acdfab598a76ce2d0306ea8
skydream <skydream2016@gmail.com>
2021-11-20 14:16:01 +0200

    rename

--- codewars.js
+++ codewars.js
@@ -245,3 +245,15 @@
 // }
 
 // console.log(digitizer(n));
+
+const arr = [-1,1,2,3,4,5,6,7,8,9];
+
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+  }
+console.log(firstNonConsecutive(arr));
